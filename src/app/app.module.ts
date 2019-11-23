91844643c64d33c39a1aa7882aa90aa3a1e92efc
Buntybru <buntybru@gmail.com>
2019-11-23 13:15:13 +0530

    Added login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {MapComponent} from './map/map.component';
+import {ListComponent} from './list/list.component';
+
+const routes: Routes = [{
+  path:'',
+  component:LoginComponent
+},
+{
+  path:'map',
+  component:MapComponent
+},
+{
+  path:'list',
+  component:ListComponent
+}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ListComponent,MapComponent,LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,RouterModule.forRoot(routes),
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
